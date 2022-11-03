1506b3902b5d6909dc2f52fdcc2dc39a9ce2a886
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2022-11-03 18:16:31 +0100

    fixed angular bug with collection and implement firebase call of server

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
-
+import { Firestore, collectionData, collection, doc } from '@angular/fire/firestore';
+import { setDoc } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+import { updateDoc } from "firebase/firestore";
 
 @Component({
   selector: 'app-game',
@@ -13,12 +16,37 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string = '';
   game: Game;
+  games$: Observable<any>
 
-  constructor(public dialog: MatDialog) { }
+  constructor(private firestore: Firestore, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
+    const coll = collection(this.firestore, 'games');
+    this.games$ = collectionData(coll);
+    this.games$.subscribe((game) => {
+      console.log('neue todos sind:', game);      
+    })
   }
+
+
+  /*  todos$: Observable<any>;
+  todos: Array<any>;
+  todotext = '';
+    constructor(private firestore: Firestore) {
+    const coll = collection(firestore, 'todos');
+    this.todos$ = collectionData(coll);
+    this.todos$.subscribe((newTodos) => {
+      console.log('neue todos sind:', newTodos);
+      this.todos = newTodos;
+    })
+  }
+
+  newTodo() {
+    const coll = collection(this.firestore, 'todos');
+    setDoc(doc(coll), { name: this.todotext })
+  };
+} */
 
   newGame() {
     this.game = new Game();
@@ -43,8 +71,8 @@ export class GameComponent implements OnInit {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
     dialogRef.afterClosed().subscribe(name => {
-      if(name && name.length > 0) 
-      this.game.players.push(name);
+      if (name && name.length > 0)
+        this.game.players.push(name);
     });
   }
 }
